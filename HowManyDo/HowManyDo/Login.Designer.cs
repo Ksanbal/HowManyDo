ecf520ffb91a7e4139ca56d44f68a9407a882232
Ksanbal <hyungyun96@gmail.com>
2019-11-11 18:40:59 +0900

    로그인 기능을 위한 폼을 생성하였습니다.

---
+++ HowManyDo/HowManyDo/Login.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace HowManyDo
+{
+	partial class Main_Form_Login
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("서울남산체 M", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.label1.Location = new System.Drawing.Point(7, 14);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 21);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "아이디";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("서울남산체 M", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.button1.Location = new System.Drawing.Point(57, 110);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(99, 31);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "로그인";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(113, 14);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(147, 21);
+			this.textBox1.TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(113, 64);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(147, 21);
+			this.textBox2.TabIndex = 5;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("서울남산체 M", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.label2.Location = new System.Drawing.Point(7, 64);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 21);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "비밀번호";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("서울남산체 M", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.button2.Location = new System.Drawing.Point(162, 110);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(99, 31);
+			this.button2.TabIndex = 6;
+			this.button2.Text = "취소";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// Main_Form_Login
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(281, 153);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.Name = "Main_Form_Login";
+			this.Text = "로그인";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button2;
+	}
+}

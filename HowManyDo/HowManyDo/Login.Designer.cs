5e24c1858cc30422811fa104ab5165dd3a5e0e6c
Ksanbal <hyungyun96@gmail.com>
2019-11-14 00:22:38 +0900

    로그인 기능을 추가하였고, 테스트 완료했습니다.

--- HowManyDo/HowManyDo/Login.Designer.cs
+++ HowManyDo/HowManyDo/Login.Designer.cs
@@ -39,10 +39,9 @@
 			// Login_Label_Id
 			// 
 			this.Login_Label_Id.Font = new System.Drawing.Font("서울남산체 M", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.Login_Label_Id.Location = new System.Drawing.Point(10, 21);
-			this.Login_Label_Id.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.Login_Label_Id.Location = new System.Drawing.Point(7, 14);
 			this.Login_Label_Id.Name = "Login_Label_Id";
-			this.Login_Label_Id.Size = new System.Drawing.Size(143, 32);
+			this.Login_Label_Id.Size = new System.Drawing.Size(100, 21);
 			this.Login_Label_Id.TabIndex = 0;
 			this.Login_Label_Id.Text = "아이디";
 			this.Login_Label_Id.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -50,10 +49,9 @@
 			// Login_Btn_Login
 			// 
 			this.Login_Btn_Login.Font = new System.Drawing.Font("서울남산체 M", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.Login_Btn_Login.Location = new System.Drawing.Point(81, 165);
-			this.Login_Btn_Login.Margin = new System.Windows.Forms.Padding(4);
+			this.Login_Btn_Login.Location = new System.Drawing.Point(57, 110);
 			this.Login_Btn_Login.Name = "Login_Btn_Login";
-			this.Login_Btn_Login.Size = new System.Drawing.Size(141, 46);
+			this.Login_Btn_Login.Size = new System.Drawing.Size(99, 31);
 			this.Login_Btn_Login.TabIndex = 1;
 			this.Login_Btn_Login.Text = "로그인";
 			this.Login_Btn_Login.UseVisualStyleBackColor = true;
@@ -61,27 +59,25 @@
 			// 
 			// Login_TextB_Id
 			// 
-			this.Login_TextB_Id.Location = new System.Drawing.Point(161, 21);
-			this.Login_TextB_Id.Margin = new System.Windows.Forms.Padding(4);
+			this.Login_TextB_Id.Location = new System.Drawing.Point(113, 14);
 			this.Login_TextB_Id.Name = "Login_TextB_Id";
-			this.Login_TextB_Id.Size = new System.Drawing.Size(208, 28);
+			this.Login_TextB_Id.Size = new System.Drawing.Size(147, 21);
 			this.Login_TextB_Id.TabIndex = 3;
 			// 
 			// Login_TextB_Passwd
 			// 
-			this.Login_TextB_Passwd.Location = new System.Drawing.Point(161, 96);
-			this.Login_TextB_Passwd.Margin = new System.Windows.Forms.Padding(4);
+			this.Login_TextB_Passwd.Location = new System.Drawing.Point(113, 64);
 			this.Login_TextB_Passwd.Name = "Login_TextB_Passwd";
-			this.Login_TextB_Passwd.Size = new System.Drawing.Size(208, 28);
+			this.Login_TextB_Passwd.PasswordChar = '*';
+			this.Login_TextB_Passwd.Size = new System.Drawing.Size(147, 21);
 			this.Login_TextB_Passwd.TabIndex = 5;
 			// 
 			// Login_Lable_Passwd
 			// 
 			this.Login_Lable_Passwd.Font = new System.Drawing.Font("서울남산체 M", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.Login_Lable_Passwd.Location = new System.Drawing.Point(10, 96);
-			this.Login_Lable_Passwd.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.Login_Lable_Passwd.Location = new System.Drawing.Point(7, 64);
 			this.Login_Lable_Passwd.Name = "Login_Lable_Passwd";
-			this.Login_Lable_Passwd.Size = new System.Drawing.Size(143, 32);
+			this.Login_Lable_Passwd.Size = new System.Drawing.Size(100, 21);
 			this.Login_Lable_Passwd.TabIndex = 4;
 			this.Login_Lable_Passwd.Text = "비밀번호";
 			this.Login_Lable_Passwd.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -89,10 +85,9 @@
 			// Login_Btn_Cancel
 			// 
 			this.Login_Btn_Cancel.Font = new System.Drawing.Font("서울남산체 M", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.Login_Btn_Cancel.Location = new System.Drawing.Point(231, 165);
-			this.Login_Btn_Cancel.Margin = new System.Windows.Forms.Padding(4);
+			this.Login_Btn_Cancel.Location = new System.Drawing.Point(162, 110);
 			this.Login_Btn_Cancel.Name = "Login_Btn_Cancel";
-			this.Login_Btn_Cancel.Size = new System.Drawing.Size(141, 46);
+			this.Login_Btn_Cancel.Size = new System.Drawing.Size(99, 31);
 			this.Login_Btn_Cancel.TabIndex = 6;
 			this.Login_Btn_Cancel.Text = "취소";
 			this.Login_Btn_Cancel.UseVisualStyleBackColor = true;
@@ -100,16 +95,15 @@
 			// 
 			// Main_Form_Login
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(401, 230);
+			this.ClientSize = new System.Drawing.Size(281, 153);
 			this.Controls.Add(this.Login_Btn_Cancel);
 			this.Controls.Add(this.Login_TextB_Passwd);
 			this.Controls.Add(this.Login_Lable_Passwd);
 			this.Controls.Add(this.Login_TextB_Id);
 			this.Controls.Add(this.Login_Btn_Login);
 			this.Controls.Add(this.Login_Label_Id);
-			this.Margin = new System.Windows.Forms.Padding(4);
 			this.Name = "Main_Form_Login";
 			this.Text = "로그인";
 			this.ResumeLayout(false);
--- HowManyDo/HowManyDo/Login.cs
+++ HowManyDo/HowManyDo/Login.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using HowManyDo.Admin;
+using HowManyDo.User;
 
 namespace HowManyDo
 {
@@ -20,7 +22,7 @@ namespace HowManyDo
 		
 		private void Login_Btn_Login_Click(object sender, EventArgs e)
 		{
-			
+			LoginCheck(Login_TextB_Id.Text, Login_TextB_Passwd.Text);
 		}
 
 		private void Login_Btn_Cancel_Click(object sender, EventArgs e)
@@ -30,43 +32,60 @@ namespace HowManyDo
 
 		// 메소드 모음
 		// Account폴더에서 입력한 ID와 같은 파일을 찾아 비밀번호가 맞는지 확인하고, 계정정보를 반환하는 메소드
-		private string SearchID(string InputId, string InputPasswd) 
+		private void LoginCheck(string InputId, string InputPasswd) 
 		{
+			string AccountPath = @"D:\HG\Programing\HowManyDo\HowManyDo\HowManyDo\bin\Accounts";
 			// Account폴더에서 입력한 ID와 같은 파일을 검색
-			DirectoryInfo DI = new DirectoryInfo("Accounts");
+			DirectoryInfo DI = new DirectoryInfo(AccountPath);
 			string RightId = "";
-
-			foreach(var item in DI.GetDirectories()) //폴더 내 모든 파일 순환
+			
+			foreach(var item in DI.GetFiles()) //폴더 내 모든 파일 순환
 			{
-				if (item.Name == InputId) // 파일명이 입력한 아이디값과 같을 때
+				if (item.Name == InputId+".txt") // 파일명이 입력한 아이디값과 같을 때
 					RightId = item.Name; // 파일명 반환
 			}
-
-			// 파일내에 비밀번호와 사용자 이름, 권한을 배열로 반환
-			StreamReader sr = new StreamReader(RightId);
-			string[] account = new string[3];
-
-			while(!sr.EndOfStream)
+			
+			if(RightId != InputId+".txt") // 아이디가 일치하지 않는 경우
 			{
-				int i = 0;
-				account[i] = sr.ReadLine();
-				i++;
+				MessageBox.Show("일치하는 아이디가 없습니다.");
 			}
+			else //아이디가 일치하는 경우
+			{
 
-			//입력된 아이디와 비밀번호가 같은지 확인하는 메소드
-			//account[0] : 비밀번호, account[1] : 사용자명, account[2] : 권한
-			if (account[0] != InputPasswd)
-				MessageBox.Show("비밀번호가 맞지 않습니다.");
-			else
+				// 파일내에 비밀번호와 사용자 이름, 권한을 배열로 반환
+				StreamReader sr = new StreamReader(AccountPath + @"\" + RightId);
+				string[] account = new string[3];
+				int i = 0;
 
+				while (i < 3)
+				{
+					account[i] = sr.ReadLine();
+					i++;
+				}
 
-			return null;
-		}
-
-		// 입력된 아이디와 비밀번호가 같은지 확인하는 메소드
-		private string CheckPasswd(string InputId, string InputPasswd)
-		{
-			return null;
+				//입력된 아이디와 비밀번호가 같은지 확인하는 메소드
+				//account[0] : 비밀번호, account[1] : 사용자명, account[2] : 권한
+				if (account[0] != InputPasswd) // 비밀번호가 틀렸을 경우
+				{
+					MessageBox.Show("비밀번호가 틀렸습니다.");
+				}
+				else
+				{
+					MessageBox.Show("로그인 성공");
+					if (account[2] == "admin")
+					{
+						Admin_Form_Main adminform = new Admin_Form_Main();
+						Close();
+						adminform.Show();
+					}
+					else
+					{
+						User_Form_Main userform = new User_Form_Main();
+						Close();
+						userform.Show();
+					}
+				}
+			}
 		}
 	}
 }

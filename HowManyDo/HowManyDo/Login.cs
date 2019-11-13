8d10f005f369edfa3a21b3a76e2c240d03841d37
Ksanbal <hyungyun96@gmail.com>
2019-11-13 17:55:39 +0900

    로그인을 위해 아이디와 비밀번호를 체크하는 메소드를 하나로 합쳤고, 계정파일 내용을 반환받는 부분을 추가했습니다.

--- HowManyDo/HowManyDo/Login.cs
+++ HowManyDo/HowManyDo/Login.cs
@@ -29,16 +29,36 @@ namespace HowManyDo
 		}
 
 		// 메소드 모음
-		// Account폴더에서 입력한 ID와 같은 파일을 찾아서 파일명을 반환합니다. 
-		private string SearchID(string InputId) 
+		// Account폴더에서 입력한 ID와 같은 파일을 찾아 비밀번호가 맞는지 확인하고, 계정정보를 반환하는 메소드
+		private string SearchID(string InputId, string InputPasswd) 
 		{
+			// Account폴더에서 입력한 ID와 같은 파일을 검색
 			DirectoryInfo DI = new DirectoryInfo("Accounts");
+			string RightId = "";
 
 			foreach(var item in DI.GetDirectories()) //폴더 내 모든 파일 순환
 			{
 				if (item.Name == InputId) // 파일명이 입력한 아이디값과 같을 때
-					return item.Name;
+					RightId = item.Name; // 파일명 반환
 			}
+
+			// 파일내에 비밀번호와 사용자 이름, 권한을 배열로 반환
+			StreamReader sr = new StreamReader(RightId);
+			string[] account = new string[3];
+
+			while(!sr.EndOfStream)
+			{
+				int i = 0;
+				account[i] = sr.ReadLine();
+				i++;
+			}
+
+			//입력된 아이디와 비밀번호가 같은지 확인하는 메소드
+			//account[0] : 비밀번호, account[1] : 사용자명, account[2] : 권한
+			if (account[0] != InputPasswd)
+				MessageBox.Show("비밀번호가 맞지 않습니다.");
+			else
+
 
 			return null;
 		}

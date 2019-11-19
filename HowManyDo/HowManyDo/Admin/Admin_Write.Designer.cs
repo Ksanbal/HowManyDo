c0b73fd8ee36d2203cce6e3fb60c5511bb241115
Ksanbal <hyungyun96@gmail.com>
2019-11-19 18:44:04 +0900

    글을 작성하는 기능을 작성했습니다.

--- HowManyDo/HowManyDo/Admin/Admin_Write.Designer.cs
+++ HowManyDo/HowManyDo/Admin/Admin_Write.Designer.cs
@@ -31,29 +31,31 @@
 			this.AWrite_TextB_Body = new System.Windows.Forms.TextBox();
 			this.AWrite_Lable_Name = new System.Windows.Forms.Label();
 			this.AWrite_Btn_SelectPic = new System.Windows.Forms.Button();
-			this.AWrtie_Pic_SelectedPic = new System.Windows.Forms.PictureBox();
+			this.AWrite_Pic_SelectedPic = new System.Windows.Forms.PictureBox();
 			this.AWrtie_Btn_Send = new System.Windows.Forms.Button();
 			this.AWrite_Btn_Temp = new System.Windows.Forms.Button();
 			this.AWrite_Btn_Cancel = new System.Windows.Forms.Button();
-			this.AWrite_Listb_Name = new System.Windows.Forms.ListBox();
-			((System.ComponentModel.ISupportInitialize)(this.AWrtie_Pic_SelectedPic)).BeginInit();
+			this.AWrite_TextB_Name = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.AWrite_Pic_SelectedPic)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// AWrite_TextB_Body
 			// 
-			this.AWrite_TextB_Body.Location = new System.Drawing.Point(13, 13);
+			this.AWrite_TextB_Body.Location = new System.Drawing.Point(24, 26);
+			this.AWrite_TextB_Body.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
 			this.AWrite_TextB_Body.Multiline = true;
 			this.AWrite_TextB_Body.Name = "AWrite_TextB_Body";
-			this.AWrite_TextB_Body.Size = new System.Drawing.Size(328, 425);
+			this.AWrite_TextB_Body.Size = new System.Drawing.Size(606, 846);
 			this.AWrite_TextB_Body.TabIndex = 0;
 			// 
 			// AWrite_Lable_Name
 			// 
 			this.AWrite_Lable_Name.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
 			this.AWrite_Lable_Name.Font = new System.Drawing.Font("서울남산체 M", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.AWrite_Lable_Name.Location = new System.Drawing.Point(348, 13);
+			this.AWrite_Lable_Name.Location = new System.Drawing.Point(646, 26);
+			this.AWrite_Lable_Name.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
 			this.AWrite_Lable_Name.Name = "AWrite_Lable_Name";
-			this.AWrite_Lable_Name.Size = new System.Drawing.Size(138, 23);
+			this.AWrite_Lable_Name.Size = new System.Drawing.Size(255, 44);
 			this.AWrite_Lable_Name.TabIndex = 2;
 			this.AWrite_Lable_Name.Text = "원생 이름";
 			this.AWrite_Lable_Name.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -61,31 +63,34 @@
 			// AWrite_Btn_SelectPic
 			// 
 			this.AWrite_Btn_SelectPic.Font = new System.Drawing.Font("서울남산체 M", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.AWrite_Btn_SelectPic.Location = new System.Drawing.Point(348, 95);
+			this.AWrite_Btn_SelectPic.Location = new System.Drawing.Point(646, 190);
+			this.AWrite_Btn_SelectPic.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
 			this.AWrite_Btn_SelectPic.Name = "AWrite_Btn_SelectPic";
-			this.AWrite_Btn_SelectPic.Size = new System.Drawing.Size(139, 23);
+			this.AWrite_Btn_SelectPic.Size = new System.Drawing.Size(258, 46);
 			this.AWrite_Btn_SelectPic.TabIndex = 3;
 			this.AWrite_Btn_SelectPic.Text = "사진 첨부";
 			this.AWrite_Btn_SelectPic.UseVisualStyleBackColor = true;
 			this.AWrite_Btn_SelectPic.Click += new System.EventHandler(this.AWrite_Btn_SelectPic_Click);
 			// 
-			// AWrtie_Pic_SelectedPic
+			// AWrite_Pic_SelectedPic
 			// 
-			this.AWrtie_Pic_SelectedPic.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.AWrtie_Pic_SelectedPic.Location = new System.Drawing.Point(348, 124);
-			this.AWrtie_Pic_SelectedPic.Name = "AWrtie_Pic_SelectedPic";
-			this.AWrtie_Pic_SelectedPic.Size = new System.Drawing.Size(138, 90);
-			this.AWrtie_Pic_SelectedPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-			this.AWrtie_Pic_SelectedPic.TabIndex = 4;
-			this.AWrtie_Pic_SelectedPic.TabStop = false;
+			this.AWrite_Pic_SelectedPic.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.AWrite_Pic_SelectedPic.Location = new System.Drawing.Point(646, 248);
+			this.AWrite_Pic_SelectedPic.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+			this.AWrite_Pic_SelectedPic.Name = "AWrite_Pic_SelectedPic";
+			this.AWrite_Pic_SelectedPic.Size = new System.Drawing.Size(255, 178);
+			this.AWrite_Pic_SelectedPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.AWrite_Pic_SelectedPic.TabIndex = 4;
+			this.AWrite_Pic_SelectedPic.TabStop = false;
 			// 
 			// AWrtie_Btn_Send
 			// 
 			this.AWrtie_Btn_Send.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
 			this.AWrtie_Btn_Send.Font = new System.Drawing.Font("서울남산체 M", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.AWrtie_Btn_Send.Location = new System.Drawing.Point(348, 245);
+			this.AWrtie_Btn_Send.Location = new System.Drawing.Point(646, 490);
+			this.AWrtie_Btn_Send.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
 			this.AWrtie_Btn_Send.Name = "AWrtie_Btn_Send";
-			this.AWrtie_Btn_Send.Size = new System.Drawing.Size(138, 53);
+			this.AWrtie_Btn_Send.Size = new System.Drawing.Size(256, 106);
 			this.AWrtie_Btn_Send.TabIndex = 5;
 			this.AWrtie_Btn_Send.Text = "보내기";
 			this.AWrtie_Btn_Send.UseVisualStyleBackColor = true;
@@ -94,9 +99,10 @@
 			// AWrite_Btn_Temp
 			// 
 			this.AWrite_Btn_Temp.Font = new System.Drawing.Font("서울남산체 M", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.AWrite_Btn_Temp.Location = new System.Drawing.Point(348, 315);
+			this.AWrite_Btn_Temp.Location = new System.Drawing.Point(646, 630);
+			this.AWrite_Btn_Temp.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
 			this.AWrite_Btn_Temp.Name = "AWrite_Btn_Temp";
-			this.AWrite_Btn_Temp.Size = new System.Drawing.Size(138, 53);
+			this.AWrite_Btn_Temp.Size = new System.Drawing.Size(256, 106);
 			this.AWrite_Btn_Temp.TabIndex = 6;
 			this.AWrite_Btn_Temp.Text = "임시저장";
 			this.AWrite_Btn_Temp.UseVisualStyleBackColor = true;
@@ -105,39 +111,40 @@
 			// AWrite_Btn_Cancel
 			// 
 			this.AWrite_Btn_Cancel.Font = new System.Drawing.Font("서울남산체 M", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.AWrite_Btn_Cancel.Location = new System.Drawing.Point(348, 385);
+			this.AWrite_Btn_Cancel.Location = new System.Drawing.Point(646, 770);
+			this.AWrite_Btn_Cancel.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
 			this.AWrite_Btn_Cancel.Name = "AWrite_Btn_Cancel";
-			this.AWrite_Btn_Cancel.Size = new System.Drawing.Size(138, 53);
+			this.AWrite_Btn_Cancel.Size = new System.Drawing.Size(256, 106);
 			this.AWrite_Btn_Cancel.TabIndex = 7;
 			this.AWrite_Btn_Cancel.Text = "취소";
 			this.AWrite_Btn_Cancel.UseVisualStyleBackColor = true;
 			this.AWrite_Btn_Cancel.Click += new System.EventHandler(this.AWrite_Btn_Cancel_Click);
 			// 
-			// AWrite_Listb_Name
+			// AWrite_TextB_Name
 			// 
-			this.AWrite_Listb_Name.FormattingEnabled = true;
-			this.AWrite_Listb_Name.ItemHeight = 12;
-			this.AWrite_Listb_Name.Location = new System.Drawing.Point(348, 43);
-			this.AWrite_Listb_Name.Name = "AWrite_Listb_Name";
-			this.AWrite_Listb_Name.Size = new System.Drawing.Size(138, 40);
-			this.AWrite_Listb_Name.TabIndex = 8;
+			this.AWrite_TextB_Name.Location = new System.Drawing.Point(646, 85);
+			this.AWrite_TextB_Name.Multiline = true;
+			this.AWrite_TextB_Name.Name = "AWrite_TextB_Name";
+			this.AWrite_TextB_Name.Size = new System.Drawing.Size(255, 63);
+			this.AWrite_TextB_Name.TabIndex = 8;
 			// 
 			// Admin_Form_Write
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(497, 450);
-			this.Controls.Add(this.AWrite_Listb_Name);
+			this.ClientSize = new System.Drawing.Size(923, 900);
+			this.Controls.Add(this.AWrite_TextB_Name);
 			this.Controls.Add(this.AWrite_Btn_Cancel);
 			this.Controls.Add(this.AWrite_Btn_Temp);
 			this.Controls.Add(this.AWrtie_Btn_Send);
-			this.Controls.Add(this.AWrtie_Pic_SelectedPic);
+			this.Controls.Add(this.AWrite_Pic_SelectedPic);
 			this.Controls.Add(this.AWrite_Btn_SelectPic);
 			this.Controls.Add(this.AWrite_TextB_Body);
 			this.Controls.Add(this.AWrite_Lable_Name);
+			this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
 			this.Name = "Admin_Form_Write";
 			this.Text = "글작성";
-			((System.ComponentModel.ISupportInitialize)(this.AWrtie_Pic_SelectedPic)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.AWrite_Pic_SelectedPic)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -148,10 +155,10 @@
 		private System.Windows.Forms.TextBox AWrite_TextB_Body;
 		private System.Windows.Forms.Label AWrite_Lable_Name;
 		private System.Windows.Forms.Button AWrite_Btn_SelectPic;
-		private System.Windows.Forms.PictureBox AWrtie_Pic_SelectedPic;
+		private System.Windows.Forms.PictureBox AWrite_Pic_SelectedPic;
 		private System.Windows.Forms.Button AWrtie_Btn_Send;
 		private System.Windows.Forms.Button AWrite_Btn_Temp;
 		private System.Windows.Forms.Button AWrite_Btn_Cancel;
-		private System.Windows.Forms.ListBox AWrite_Listb_Name;
+		private System.Windows.Forms.TextBox AWrite_TextB_Name;
 	}
 }
--- HowManyDo/HowManyDo/Admin/Admin_Write.cs
+++ HowManyDo/HowManyDo/Admin/Admin_Write.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.IO;
+
 namespace HowManyDo.Admin
 {
 	public partial class Admin_Form_Write : Form
@@ -17,18 +19,28 @@ namespace HowManyDo.Admin
 			InitializeComponent();
 		}
 
+		// 이미지 경로를 저장해둘 변수
+		private string imagepath;
+
 
 
 		private void AWrite_Btn_SelectPic_Click(object sender, EventArgs e)
 		{
-			MessageBox.Show("사진첨부");
+			imagepath = LoadPic();
 		}
-
 
 
 		private void AWrtie_Btn_Send_Click(object sender, EventArgs e)
 		{
-			MessageBox.Show("보내기");
+			if(AWrite_TextB_Name.Text == "" || AWrite_TextB_Body.Text == "" || AWrite_Pic_SelectedPic.Image == null)
+			{
+				MessageBox.Show("아직 작성되지 않은 칸이 있습니다");
+			}
+			else
+			{
+				SaveWrite(AWrite_TextB_Name.Text, DateTime.Now.ToString("yyyyMMdd"), AWrite_TextB_Body.Text, imagepath);
+				Close();
+			}
 		}
 
 
@@ -39,7 +51,6 @@ namespace HowManyDo.Admin
 		}
 
 
-
 		private void AWrite_Btn_Cancel_Click(object sender, EventArgs e)
 		{
 			if(MessageBox.Show("정말 취소하시겠습니까?", "", MessageBoxButtons.YesNo) == DialogResult.Yes)
@@ -48,6 +59,45 @@ namespace HowManyDo.Admin
 			}
 			else
 			{ }
+		}
+
+		// 메소드
+		private string LoadPic()
+		{
+			// 파일탐색기로 사진을 찾아서 사진 경로 반환
+			string image = "";
+			OpenFileDialog ofd = new OpenFileDialog();
+			ofd.InitialDirectory = @"D:\";
+
+			if(ofd.ShowDialog() == DialogResult.OK)
+			{
+				image = ofd.FileName;
+			}
+			else
+			{
+				return null;
+			}
+
+			// 사진 폼에 사진 첨부
+			AWrite_Pic_SelectedPic.Image = Bitmap.FromFile(image); 
+			AWrite_Pic_SelectedPic.SizeMode = PictureBoxSizeMode.StretchImage;
+
+			return image;
+		} // LoadPic()
+
+		private void SaveWrite(string name, string date,string body, string image)
+		{
+			// 텍스트 파일 작성
+			StreamWriter sw = new StreamWriter(@"D:\HG\Programing\HowManyDo\HowManyDo\HowManyDo\bin\Write\"+date+"_"+name+@".txt");
+			sw.WriteLine(name, Encoding.Default);
+			sw.WriteLine(date, Encoding.Default);
+			sw.WriteLine(body, Encoding.Default);
+			sw.Close();
+
+
+			// 사진을 Write폴더로 복사
+			FileInfo fi = new FileInfo(image);
+			fi.CopyTo(@"D:\HG\Programing\HowManyDo\HowManyDo\HowManyDo\bin\Write\"+date+"_"+name+".jpg", true);
 		}
 	}
 }

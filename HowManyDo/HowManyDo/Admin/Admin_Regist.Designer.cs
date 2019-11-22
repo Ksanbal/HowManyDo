3b88da5f2b7f9b2279da3a9775a25995ca2503f4
Ksanbal <hyungyun96@gmail.com>
2019-11-22 12:46:53 +0900

    아이디와 비밀번호를 삭제하고 이메일로 대체하였습니다.

--- HowManyDo/HowManyDo/Admin/Admin_Regist.Designer.cs
+++ HowManyDo/HowManyDo/Admin/Admin_Regist.Designer.cs
@@ -34,10 +34,8 @@
 			this.ARegist_TextB_Birth = new System.Windows.Forms.TextBox();
 			this.ARegist_Label_Pname = new System.Windows.Forms.Label();
 			this.ARegist_TextB_Pname = new System.Windows.Forms.TextBox();
-			this.ARegist_Label_Id = new System.Windows.Forms.Label();
-			this.ARegist_TextB_Id = new System.Windows.Forms.TextBox();
-			this.ARegist_Label_Passwd = new System.Windows.Forms.Label();
-			this.ARegist_TextB_Passwd = new System.Windows.Forms.TextBox();
+			this.ARegist_Label_Email = new System.Windows.Forms.Label();
+			this.ARegist_TextB_Email = new System.Windows.Forms.TextBox();
 			this.ARegist_Btn_Ok = new System.Windows.Forms.Button();
 			this.ARegist_Btn_Cancel = new System.Windows.Forms.Button();
 			this.ARegist_ProgressBar = new System.Windows.Forms.ProgressBar();
@@ -45,19 +43,17 @@
 			// 
 			// ARegist_TextB_Sname
 			// 
-			this.ARegist_TextB_Sname.Location = new System.Drawing.Point(197, 42);
-			this.ARegist_TextB_Sname.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+			this.ARegist_TextB_Sname.Location = new System.Drawing.Point(106, 21);
 			this.ARegist_TextB_Sname.Name = "ARegist_TextB_Sname";
-			this.ARegist_TextB_Sname.Size = new System.Drawing.Size(182, 35);
+			this.ARegist_TextB_Sname.Size = new System.Drawing.Size(100, 21);
 			this.ARegist_TextB_Sname.TabIndex = 0;
 			// 
 			// ARegist_Label_Sname
 			// 
 			this.ARegist_Label_Sname.Font = new System.Drawing.Font("서울남산체 M", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.ARegist_Label_Sname.Location = new System.Drawing.Point(19, 42);
-			this.ARegist_Label_Sname.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+			this.ARegist_Label_Sname.Location = new System.Drawing.Point(10, 21);
 			this.ARegist_Label_Sname.Name = "ARegist_Label_Sname";
-			this.ARegist_Label_Sname.Size = new System.Drawing.Size(167, 42);
+			this.ARegist_Label_Sname.Size = new System.Drawing.Size(90, 21);
 			this.ARegist_Label_Sname.TabIndex = 1;
 			this.ARegist_Label_Sname.Text = "학생 이름";
 			this.ARegist_Label_Sname.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -65,86 +61,60 @@
 			// ARegist_Label_Birth
 			// 
 			this.ARegist_Label_Birth.Font = new System.Drawing.Font("서울남산체 M", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.ARegist_Label_Birth.Location = new System.Drawing.Point(19, 124);
-			this.ARegist_Label_Birth.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+			this.ARegist_Label_Birth.Location = new System.Drawing.Point(10, 62);
 			this.ARegist_Label_Birth.Name = "ARegist_Label_Birth";
-			this.ARegist_Label_Birth.Size = new System.Drawing.Size(167, 42);
+			this.ARegist_Label_Birth.Size = new System.Drawing.Size(90, 21);
 			this.ARegist_Label_Birth.TabIndex = 3;
 			this.ARegist_Label_Birth.Text = "생년월일";
 			this.ARegist_Label_Birth.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
 			// ARegist_TextB_Birth
 			// 
-			this.ARegist_TextB_Birth.Location = new System.Drawing.Point(197, 124);
-			this.ARegist_TextB_Birth.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+			this.ARegist_TextB_Birth.Location = new System.Drawing.Point(106, 62);
 			this.ARegist_TextB_Birth.Name = "ARegist_TextB_Birth";
-			this.ARegist_TextB_Birth.Size = new System.Drawing.Size(182, 35);
+			this.ARegist_TextB_Birth.Size = new System.Drawing.Size(100, 21);
 			this.ARegist_TextB_Birth.TabIndex = 2;
 			// 
 			// ARegist_Label_Pname
 			// 
 			this.ARegist_Label_Pname.Font = new System.Drawing.Font("서울남산체 M", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.ARegist_Label_Pname.Location = new System.Drawing.Point(19, 252);
-			this.ARegist_Label_Pname.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+			this.ARegist_Label_Pname.Location = new System.Drawing.Point(10, 126);
 			this.ARegist_Label_Pname.Name = "ARegist_Label_Pname";
-			this.ARegist_Label_Pname.Size = new System.Drawing.Size(167, 42);
+			this.ARegist_Label_Pname.Size = new System.Drawing.Size(90, 21);
 			this.ARegist_Label_Pname.TabIndex = 5;
 			this.ARegist_Label_Pname.Text = "학부모 성함";
 			this.ARegist_Label_Pname.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
 			// ARegist_TextB_Pname
 			// 
-			this.ARegist_TextB_Pname.Location = new System.Drawing.Point(198, 252);
-			this.ARegist_TextB_Pname.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+			this.ARegist_TextB_Pname.Location = new System.Drawing.Point(107, 126);
 			this.ARegist_TextB_Pname.Name = "ARegist_TextB_Pname";
-			this.ARegist_TextB_Pname.Size = new System.Drawing.Size(182, 35);
+			this.ARegist_TextB_Pname.Size = new System.Drawing.Size(100, 21);
 			this.ARegist_TextB_Pname.TabIndex = 4;
 			// 
-			// ARegist_Label_Id
+			// ARegist_Label_Email
 			// 
-			this.ARegist_Label_Id.Font = new System.Drawing.Font("서울남산체 M", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.ARegist_Label_Id.Location = new System.Drawing.Point(19, 336);
-			this.ARegist_Label_Id.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
-			this.ARegist_Label_Id.Name = "ARegist_Label_Id";
-			this.ARegist_Label_Id.Size = new System.Drawing.Size(167, 42);
-			this.ARegist_Label_Id.TabIndex = 7;
-			this.ARegist_Label_Id.Text = "아이디";
-			this.ARegist_Label_Id.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.ARegist_Label_Email.Font = new System.Drawing.Font("서울남산체 M", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.ARegist_Label_Email.Location = new System.Drawing.Point(10, 168);
+			this.ARegist_Label_Email.Name = "ARegist_Label_Email";
+			this.ARegist_Label_Email.Size = new System.Drawing.Size(90, 21);
+			this.ARegist_Label_Email.TabIndex = 7;
+			this.ARegist_Label_Email.Text = "이메일";
+			this.ARegist_Label_Email.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
-			// ARegist_TextB_Id
+			// ARegist_TextB_Email
 			// 
-			this.ARegist_TextB_Id.Location = new System.Drawing.Point(197, 336);
-			this.ARegist_TextB_Id.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-			this.ARegist_TextB_Id.Name = "ARegist_TextB_Id";
-			this.ARegist_TextB_Id.Size = new System.Drawing.Size(182, 35);
-			this.ARegist_TextB_Id.TabIndex = 6;
-			// 
-			// ARegist_Label_Passwd
-			// 
-			this.ARegist_Label_Passwd.Font = new System.Drawing.Font("서울남산체 M", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.ARegist_Label_Passwd.Location = new System.Drawing.Point(19, 418);
-			this.ARegist_Label_Passwd.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
-			this.ARegist_Label_Passwd.Name = "ARegist_Label_Passwd";
-			this.ARegist_Label_Passwd.Size = new System.Drawing.Size(167, 42);
-			this.ARegist_Label_Passwd.TabIndex = 9;
-			this.ARegist_Label_Passwd.Text = "비밀번호";
-			this.ARegist_Label_Passwd.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-			// 
-			// ARegist_TextB_Passwd
-			// 
-			this.ARegist_TextB_Passwd.Location = new System.Drawing.Point(197, 418);
-			this.ARegist_TextB_Passwd.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-			this.ARegist_TextB_Passwd.Name = "ARegist_TextB_Passwd";
-			this.ARegist_TextB_Passwd.Size = new System.Drawing.Size(182, 35);
-			this.ARegist_TextB_Passwd.TabIndex = 8;
+			this.ARegist_TextB_Email.Location = new System.Drawing.Point(106, 168);
+			this.ARegist_TextB_Email.Name = "ARegist_TextB_Email";
+			this.ARegist_TextB_Email.Size = new System.Drawing.Size(100, 21);
+			this.ARegist_TextB_Email.TabIndex = 6;
 			// 
 			// ARegist_Btn_Ok
 			// 
 			this.ARegist_Btn_Ok.Font = new System.Drawing.Font("서울남산체 M", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.ARegist_Btn_Ok.Location = new System.Drawing.Point(425, 124);
-			this.ARegist_Btn_Ok.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+			this.ARegist_Btn_Ok.Location = new System.Drawing.Point(229, 62);
 			this.ARegist_Btn_Ok.Name = "ARegist_Btn_Ok";
-			this.ARegist_Btn_Ok.Size = new System.Drawing.Size(288, 74);
+			this.ARegist_Btn_Ok.Size = new System.Drawing.Size(155, 37);
 			this.ARegist_Btn_Ok.TabIndex = 10;
 			this.ARegist_Btn_Ok.Text = "등록";
 			this.ARegist_Btn_Ok.UseVisualStyleBackColor = true;
@@ -153,10 +123,9 @@
 			// ARegist_Btn_Cancel
 			// 
 			this.ARegist_Btn_Cancel.Font = new System.Drawing.Font("서울남산체 M", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.ARegist_Btn_Cancel.Location = new System.Drawing.Point(425, 232);
-			this.ARegist_Btn_Cancel.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+			this.ARegist_Btn_Cancel.Location = new System.Drawing.Point(229, 116);
 			this.ARegist_Btn_Cancel.Name = "ARegist_Btn_Cancel";
-			this.ARegist_Btn_Cancel.Size = new System.Drawing.Size(288, 74);
+			this.ARegist_Btn_Cancel.Size = new System.Drawing.Size(155, 37);
 			this.ARegist_Btn_Cancel.TabIndex = 11;
 			this.ARegist_Btn_Cancel.Text = "나가기";
 			this.ARegist_Btn_Cancel.UseVisualStyleBackColor = true;
@@ -164,31 +133,27 @@
 			// 
 			// ARegist_ProgressBar
 			// 
-			this.ARegist_ProgressBar.Location = new System.Drawing.Point(425, 42);
-			this.ARegist_ProgressBar.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+			this.ARegist_ProgressBar.Location = new System.Drawing.Point(229, 21);
 			this.ARegist_ProgressBar.Name = "ARegist_ProgressBar";
-			this.ARegist_ProgressBar.Size = new System.Drawing.Size(288, 42);
+			this.ARegist_ProgressBar.Size = new System.Drawing.Size(155, 21);
 			this.ARegist_ProgressBar.TabIndex = 12;
 			// 
 			// Admin_Form_Regist
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(735, 510);
+			this.ClientSize = new System.Drawing.Size(396, 210);
 			this.Controls.Add(this.ARegist_ProgressBar);
 			this.Controls.Add(this.ARegist_Btn_Cancel);
 			this.Controls.Add(this.ARegist_Btn_Ok);
-			this.Controls.Add(this.ARegist_Label_Passwd);
-			this.Controls.Add(this.ARegist_TextB_Passwd);
-			this.Controls.Add(this.ARegist_Label_Id);
-			this.Controls.Add(this.ARegist_TextB_Id);
+			this.Controls.Add(this.ARegist_Label_Email);
+			this.Controls.Add(this.ARegist_TextB_Email);
 			this.Controls.Add(this.ARegist_Label_Pname);
 			this.Controls.Add(this.ARegist_TextB_Pname);
 			this.Controls.Add(this.ARegist_Label_Birth);
 			this.Controls.Add(this.ARegist_TextB_Birth);
 			this.Controls.Add(this.ARegist_Label_Sname);
 			this.Controls.Add(this.ARegist_TextB_Sname);
-			this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
 			this.Name = "Admin_Form_Regist";
 			this.Text = "원생 등록";
 			this.ResumeLayout(false);
@@ -204,10 +169,8 @@
 		private System.Windows.Forms.TextBox ARegist_TextB_Birth;
 		private System.Windows.Forms.Label ARegist_Label_Pname;
 		private System.Windows.Forms.TextBox ARegist_TextB_Pname;
-		private System.Windows.Forms.Label ARegist_Label_Id;
-		private System.Windows.Forms.TextBox ARegist_TextB_Id;
-		private System.Windows.Forms.Label ARegist_Label_Passwd;
-		private System.Windows.Forms.TextBox ARegist_TextB_Passwd;
+		private System.Windows.Forms.Label ARegist_Label_Email;
+		private System.Windows.Forms.TextBox ARegist_TextB_Email;
 		private System.Windows.Forms.Button ARegist_Btn_Ok;
 		private System.Windows.Forms.Button ARegist_Btn_Cancel;
 		private System.Windows.Forms.ProgressBar ARegist_ProgressBar;
--- HowManyDo/HowManyDo/Admin/Admin_Regist.cs
+++ HowManyDo/HowManyDo/Admin/Admin_Regist.cs
@@ -22,25 +22,24 @@ namespace HowManyDo.Admin
 
 		private void ARegist_Btn_Ok_Click(object sender, EventArgs e)
 		{
-			if (ARegist_TextB_Id.Text == "")
+			if (ARegist_TextB_Email.Text == "")
 			{
 				MessageBox.Show("아이디를 작성해주시기 바랍니다.");
 			} // 아이디가 비어져있는 경우 
-			else if (ARegist_TextB_Sname.Text == "" || ARegist_TextB_Birth.Text == "" || ARegist_TextB_Pname.Text == "" || 
-				ARegist_TextB_Passwd.Text == "")
+			else if (ARegist_TextB_Sname.Text == "" || ARegist_TextB_Birth.Text == "" || ARegist_TextB_Pname.Text == "")
 			{
 				if(MessageBox.Show("아직 채워지지 않은 칸이 있습니다. 정말 등록하시겠습니까?","",MessageBoxButtons.YesNo)==DialogResult.Yes)
 				{
 					// 등록 기능 구현
 					RegistNewAccount(ARegist_TextB_Sname.Text, ARegist_TextB_Birth.Text, ARegist_TextB_Pname.Text,
-						ARegist_TextB_Id.Text, ARegist_TextB_Passwd.Text, "user");
+						ARegist_TextB_Email.Text, "user");
 				}
 			} // TextBox중 하나라도 비어 있는 경우
 			else
 			{
 				// 등록 기능 구현
 				RegistNewAccount(ARegist_TextB_Sname.Text, ARegist_TextB_Birth.Text, ARegist_TextB_Pname.Text, 
-					ARegist_TextB_Id.Text, ARegist_TextB_Passwd.Text, "user");
+					ARegist_TextB_Email.Text, "user");
 			}
 		}
 
@@ -55,15 +54,15 @@ namespace HowManyDo.Admin
 			{ }
 		}
 
-		private void RegistNewAccount(string sname, string birth, string pname, string id, string passwd, string permission)
+		private void RegistNewAccount(string sname, string birth, string pname, string email, string permission)
 		{
-			string[] account = { sname, birth, pname, id, passwd, permission};
+			string[] account = { sname, birth, pname, email, permission};
 
 			// 같은 아이디가 있는지 확인
 			DirectoryInfo di = new DirectoryInfo("Accounts");
 			foreach(var item in di.GetFiles())
 			{
-				if(item.Name == id+".txt")
+				if(item.Name == email + ".txt")
 				{
 					MessageBox.Show("이미 존재하는 아이디입니다.");
 					return;
@@ -71,7 +70,7 @@ namespace HowManyDo.Admin
 			}
 
 			// 학생 정보가 담긴 파일을 새로 작성
-			StreamWriter sw = new StreamWriter(@"Accounts\" + id + ".txt");
+			StreamWriter sw = new StreamWriter(@"Accounts\" + email + ".txt");
 			
 			foreach(string i in account)
 			{

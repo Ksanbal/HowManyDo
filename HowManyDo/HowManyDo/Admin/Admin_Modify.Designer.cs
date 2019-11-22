7998029e0ee156df81debfabdb3c7f69dd0c739a
Ksanbal <hyungyun96@gmail.com>
2019-11-22 12:47:55 +0900

    아이디와 비밀번호를 삭제하고 이메일로 대체하였습니다.

--- HowManyDo/HowManyDo/Admin/Admin_Modify.Designer.cs
+++ HowManyDo/HowManyDo/Admin/Admin_Modify.Designer.cs
@@ -31,34 +31,29 @@
 			this.AModify_ProgressBar = new System.Windows.Forms.ProgressBar();
 			this.AModify_Btn_Cancel = new System.Windows.Forms.Button();
 			this.AModify_Btn_Ok = new System.Windows.Forms.Button();
-			this.AModify_Label_Passwd = new System.Windows.Forms.Label();
-			this.AModify_TextB_Passwd = new System.Windows.Forms.TextBox();
-			this.AModify_Label_Id = new System.Windows.Forms.Label();
-			this.AModify_TextB_Id = new System.Windows.Forms.TextBox();
+			this.AModify_Label_Email = new System.Windows.Forms.Label();
+			this.AModify_TextB_Email = new System.Windows.Forms.TextBox();
 			this.AModify_Label_Pname = new System.Windows.Forms.Label();
 			this.AModify_TextB_Pname = new System.Windows.Forms.TextBox();
 			this.AModify_Label_Birth = new System.Windows.Forms.Label();
 			this.AModify_TextB_Birth = new System.Windows.Forms.TextBox();
 			this.AModify_Label_Sname = new System.Windows.Forms.Label();
 			this.AModify_TextB_Sname = new System.Windows.Forms.TextBox();
-			this.label1 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// AModify_ProgressBar
 			// 
-			this.AModify_ProgressBar.Location = new System.Drawing.Point(425, 42);
-			this.AModify_ProgressBar.Margin = new System.Windows.Forms.Padding(6);
+			this.AModify_ProgressBar.Location = new System.Drawing.Point(229, 21);
 			this.AModify_ProgressBar.Name = "AModify_ProgressBar";
-			this.AModify_ProgressBar.Size = new System.Drawing.Size(288, 42);
+			this.AModify_ProgressBar.Size = new System.Drawing.Size(155, 21);
 			this.AModify_ProgressBar.TabIndex = 25;
 			// 
 			// AModify_Btn_Cancel
 			// 
 			this.AModify_Btn_Cancel.Font = new System.Drawing.Font("서울남산체 M", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.AModify_Btn_Cancel.Location = new System.Drawing.Point(425, 232);
-			this.AModify_Btn_Cancel.Margin = new System.Windows.Forms.Padding(6);
+			this.AModify_Btn_Cancel.Location = new System.Drawing.Point(229, 116);
 			this.AModify_Btn_Cancel.Name = "AModify_Btn_Cancel";
-			this.AModify_Btn_Cancel.Size = new System.Drawing.Size(288, 74);
+			this.AModify_Btn_Cancel.Size = new System.Drawing.Size(155, 37);
 			this.AModify_Btn_Cancel.TabIndex = 24;
 			this.AModify_Btn_Cancel.Text = "나가기";
 			this.AModify_Btn_Cancel.UseVisualStyleBackColor = true;
@@ -67,138 +62,98 @@
 			// AModify_Btn_Ok
 			// 
 			this.AModify_Btn_Ok.Font = new System.Drawing.Font("서울남산체 M", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.AModify_Btn_Ok.Location = new System.Drawing.Point(425, 124);
-			this.AModify_Btn_Ok.Margin = new System.Windows.Forms.Padding(6);
+			this.AModify_Btn_Ok.Location = new System.Drawing.Point(229, 62);
 			this.AModify_Btn_Ok.Name = "AModify_Btn_Ok";
-			this.AModify_Btn_Ok.Size = new System.Drawing.Size(288, 74);
+			this.AModify_Btn_Ok.Size = new System.Drawing.Size(155, 37);
 			this.AModify_Btn_Ok.TabIndex = 23;
 			this.AModify_Btn_Ok.Text = "수정";
 			this.AModify_Btn_Ok.UseVisualStyleBackColor = true;
 			this.AModify_Btn_Ok.Click += new System.EventHandler(this.AModify_Btn_Ok_Click);
 			// 
-			// AModify_Label_Passwd
+			// AModify_Label_Email
 			// 
-			this.AModify_Label_Passwd.Font = new System.Drawing.Font("서울남산체 M", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.AModify_Label_Passwd.Location = new System.Drawing.Point(19, 418);
-			this.AModify_Label_Passwd.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
-			this.AModify_Label_Passwd.Name = "AModify_Label_Passwd";
-			this.AModify_Label_Passwd.Size = new System.Drawing.Size(167, 42);
-			this.AModify_Label_Passwd.TabIndex = 22;
-			this.AModify_Label_Passwd.Text = "비밀번호";
-			this.AModify_Label_Passwd.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.AModify_Label_Email.Font = new System.Drawing.Font("서울남산체 M", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.AModify_Label_Email.Location = new System.Drawing.Point(10, 168);
+			this.AModify_Label_Email.Name = "AModify_Label_Email";
+			this.AModify_Label_Email.Size = new System.Drawing.Size(90, 21);
+			this.AModify_Label_Email.TabIndex = 20;
+			this.AModify_Label_Email.Text = "이메일";
+			this.AModify_Label_Email.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
-			// AModify_TextB_Passwd
+			// AModify_TextB_Email
 			// 
-			this.AModify_TextB_Passwd.Location = new System.Drawing.Point(197, 418);
-			this.AModify_TextB_Passwd.Margin = new System.Windows.Forms.Padding(6);
-			this.AModify_TextB_Passwd.Name = "AModify_TextB_Passwd";
-			this.AModify_TextB_Passwd.Size = new System.Drawing.Size(182, 35);
-			this.AModify_TextB_Passwd.TabIndex = 21;
-			// 
-			// AModify_Label_Id
-			// 
-			this.AModify_Label_Id.Font = new System.Drawing.Font("서울남산체 M", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.AModify_Label_Id.Location = new System.Drawing.Point(19, 336);
-			this.AModify_Label_Id.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
-			this.AModify_Label_Id.Name = "AModify_Label_Id";
-			this.AModify_Label_Id.Size = new System.Drawing.Size(167, 42);
-			this.AModify_Label_Id.TabIndex = 20;
-			this.AModify_Label_Id.Text = "아이디";
-			this.AModify_Label_Id.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-			// 
-			// AModify_TextB_Id
-			// 
-			this.AModify_TextB_Id.Location = new System.Drawing.Point(197, 336);
-			this.AModify_TextB_Id.Margin = new System.Windows.Forms.Padding(6);
-			this.AModify_TextB_Id.Name = "AModify_TextB_Id";
-			this.AModify_TextB_Id.Size = new System.Drawing.Size(182, 35);
-			this.AModify_TextB_Id.TabIndex = 19;
+			this.AModify_TextB_Email.Location = new System.Drawing.Point(106, 168);
+			this.AModify_TextB_Email.Name = "AModify_TextB_Email";
+			this.AModify_TextB_Email.Size = new System.Drawing.Size(100, 21);
+			this.AModify_TextB_Email.TabIndex = 19;
 			// 
 			// AModify_Label_Pname
 			// 
 			this.AModify_Label_Pname.Font = new System.Drawing.Font("서울남산체 M", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.AModify_Label_Pname.Location = new System.Drawing.Point(19, 252);
-			this.AModify_Label_Pname.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+			this.AModify_Label_Pname.Location = new System.Drawing.Point(10, 126);
 			this.AModify_Label_Pname.Name = "AModify_Label_Pname";
-			this.AModify_Label_Pname.Size = new System.Drawing.Size(167, 42);
+			this.AModify_Label_Pname.Size = new System.Drawing.Size(90, 21);
 			this.AModify_Label_Pname.TabIndex = 18;
 			this.AModify_Label_Pname.Text = "학부모 성함";
 			this.AModify_Label_Pname.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
 			// AModify_TextB_Pname
 			// 
-			this.AModify_TextB_Pname.Location = new System.Drawing.Point(197, 252);
-			this.AModify_TextB_Pname.Margin = new System.Windows.Forms.Padding(6);
+			this.AModify_TextB_Pname.Location = new System.Drawing.Point(106, 126);
 			this.AModify_TextB_Pname.Name = "AModify_TextB_Pname";
-			this.AModify_TextB_Pname.Size = new System.Drawing.Size(182, 35);
+			this.AModify_TextB_Pname.Size = new System.Drawing.Size(100, 21);
 			this.AModify_TextB_Pname.TabIndex = 17;
 			// 
 			// AModify_Label_Birth
 			// 
 			this.AModify_Label_Birth.Font = new System.Drawing.Font("서울남산체 M", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.AModify_Label_Birth.Location = new System.Drawing.Point(19, 124);
-			this.AModify_Label_Birth.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+			this.AModify_Label_Birth.Location = new System.Drawing.Point(10, 62);
 			this.AModify_Label_Birth.Name = "AModify_Label_Birth";
-			this.AModify_Label_Birth.Size = new System.Drawing.Size(167, 42);
+			this.AModify_Label_Birth.Size = new System.Drawing.Size(90, 21);
 			this.AModify_Label_Birth.TabIndex = 16;
 			this.AModify_Label_Birth.Text = "생년월일";
 			this.AModify_Label_Birth.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
 			// AModify_TextB_Birth
 			// 
-			this.AModify_TextB_Birth.Location = new System.Drawing.Point(197, 124);
-			this.AModify_TextB_Birth.Margin = new System.Windows.Forms.Padding(6);
+			this.AModify_TextB_Birth.Location = new System.Drawing.Point(106, 62);
 			this.AModify_TextB_Birth.Name = "AModify_TextB_Birth";
-			this.AModify_TextB_Birth.Size = new System.Drawing.Size(182, 35);
+			this.AModify_TextB_Birth.Size = new System.Drawing.Size(100, 21);
 			this.AModify_TextB_Birth.TabIndex = 15;
 			// 
 			// AModify_Label_Sname
 			// 
 			this.AModify_Label_Sname.Font = new System.Drawing.Font("서울남산체 M", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-			this.AModify_Label_Sname.Location = new System.Drawing.Point(19, 42);
-			this.AModify_Label_Sname.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+			this.AModify_Label_Sname.Location = new System.Drawing.Point(10, 21);
 			this.AModify_Label_Sname.Name = "AModify_Label_Sname";
-			this.AModify_Label_Sname.Size = new System.Drawing.Size(167, 42);
+			this.AModify_Label_Sname.Size = new System.Drawing.Size(90, 21);
 			this.AModify_Label_Sname.TabIndex = 14;
 			this.AModify_Label_Sname.Text = "학생 이름";
 			this.AModify_Label_Sname.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
 			// AModify_TextB_Sname
 			// 
-			this.AModify_TextB_Sname.Location = new System.Drawing.Point(197, 42);
-			this.AModify_TextB_Sname.Margin = new System.Windows.Forms.Padding(6);
+			this.AModify_TextB_Sname.Location = new System.Drawing.Point(106, 21);
 			this.AModify_TextB_Sname.Name = "AModify_TextB_Sname";
-			this.AModify_TextB_Sname.Size = new System.Drawing.Size(182, 35);
+			this.AModify_TextB_Sname.Size = new System.Drawing.Size(100, 21);
 			this.AModify_TextB_Sname.TabIndex = 13;
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(425, 353);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(0, 24);
-			this.label1.TabIndex = 26;
 			// 
 			// Admin_Form_Modify
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(735, 510);
-			this.Controls.Add(this.label1);
+			this.ClientSize = new System.Drawing.Size(396, 210);
 			this.Controls.Add(this.AModify_ProgressBar);
 			this.Controls.Add(this.AModify_Btn_Cancel);
 			this.Controls.Add(this.AModify_Btn_Ok);
-			this.Controls.Add(this.AModify_Label_Passwd);
-			this.Controls.Add(this.AModify_TextB_Passwd);
-			this.Controls.Add(this.AModify_Label_Id);
-			this.Controls.Add(this.AModify_TextB_Id);
+			this.Controls.Add(this.AModify_Label_Email);
+			this.Controls.Add(this.AModify_TextB_Email);
 			this.Controls.Add(this.AModify_Label_Pname);
 			this.Controls.Add(this.AModify_TextB_Pname);
 			this.Controls.Add(this.AModify_Label_Birth);
 			this.Controls.Add(this.AModify_TextB_Birth);
 			this.Controls.Add(this.AModify_Label_Sname);
 			this.Controls.Add(this.AModify_TextB_Sname);
-			this.Margin = new System.Windows.Forms.Padding(6);
 			this.Name = "Admin_Form_Modify";
 			this.Text = "정보 수정";
 			this.Load += new System.EventHandler(this.Admin_Form_Modify_Load);
@@ -212,16 +167,13 @@
 		private System.Windows.Forms.ProgressBar AModify_ProgressBar;
 		private System.Windows.Forms.Button AModify_Btn_Cancel;
 		private System.Windows.Forms.Button AModify_Btn_Ok;
-		private System.Windows.Forms.Label AModify_Label_Passwd;
-		private System.Windows.Forms.TextBox AModify_TextB_Passwd;
-		private System.Windows.Forms.Label AModify_Label_Id;
-		private System.Windows.Forms.TextBox AModify_TextB_Id;
+		private System.Windows.Forms.Label AModify_Label_Email;
+		private System.Windows.Forms.TextBox AModify_TextB_Email;
 		private System.Windows.Forms.Label AModify_Label_Pname;
 		private System.Windows.Forms.TextBox AModify_TextB_Pname;
 		private System.Windows.Forms.Label AModify_Label_Birth;
 		private System.Windows.Forms.TextBox AModify_TextB_Birth;
 		private System.Windows.Forms.Label AModify_Label_Sname;
 		private System.Windows.Forms.TextBox AModify_TextB_Sname;
-		private System.Windows.Forms.Label label1;
 	}
 }
